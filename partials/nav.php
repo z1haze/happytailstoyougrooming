518f06dfe8c6d00fa96a7d33ff7ae0a5496d7ad5
Stephen Hendricks <shendricks@crownawards.com>
2019-07-13 17:42:37 -0700

    add php extension to nav links

--- partials/nav.php
+++ partials/nav.php
@@ -4,16 +4,16 @@
       <nav class="responsive-nav" id="js-responsive-nav">
         <ul class="responsive-nav__list">
           <li class="responsive-nav__item active" id="menu-home">
-            <a href="index" class="responsive-nav__link">Home</a>
+            <a href="index.php" class="responsive-nav__link">Home</a>
           </li>
           <li class="responsive-nav__item">
-            <a href="about" class="responsive-nav__link">About</a>
+            <a href="about.php" class="responsive-nav__link">About</a>
           </li>
           <li class="responsive-nav__item">
-            <a href="services" class="responsive-nav__link">Services</a>
+            <a href="services.php" class="responsive-nav__link">Services</a>
           </li>
           <li class="responsive-nav__item">
-            <a href="contact" class="responsive-nav__link">Contact</a>
+            <a href="contact.php" class="responsive-nav__link">Contact</a>
           </li>
         </ul>
       </nav>
